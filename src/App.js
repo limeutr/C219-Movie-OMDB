f3c052b6aa2fbfb5a0a74395e6d06ff772a711a6
limeutr <23021242@myrp.edu.sg>
2025-01-08 22:54:01 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
+import { use } from "react";
 import { useEffect, useState } from "react";
-const KEY = "";
+const KEY = "a5db3016";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
 
-  fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=batman`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+  // useEffect(() => {
+  //   const controller = new AbortController(); // Create an AbortController instance
+
+  //   if (query.trim() === "") return; // Skip fetching if query is empty
+
+  //   fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal:controller.signal})
+  //     .then((res) => res.json())
+  //     .then((data) => data.Response === "True" && setMovies(data.Search))
+  //     .catch((err)=> console.log(err));
+
+  //     return () => controller.abort();
+  //   }, [query]);
+
+  useEffect(() => { 
+    const controller = new AbortController(); // Create an AbortController instance
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, 
+          { signal: controller.signal, 
+          }
+        )
+        const data = await response.json();
+        if (data.Response === "True") {
+          setMovies(data.Search);
+        }
+      } catch (error) {
+        console.error("Fetch error:", error.message);
+      }
+    };
+    fetchMovies();
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
+  // useEffect(() => {
+  //  if (userName !== "") {
+  //   localStorage.setItem("savedUserName", userName);
+  //  }
+  // });
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
 
   return (
     <div>
       <h1>Movies</h1>
-      <input type="text" placeholder="Search movies..." />
+      <input
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={handleChange}
+      />
       <table>
         <thead>
           <tr>
@@ -20,12 +69,18 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {movies.map((movie) => (
-            <tr key={movie.imdbID}>
-              <td>{movie.Title}</td>
-              <td>{movie.Year}</td>
+          {movies.length > 0 ? (
+            movies.map((movie) => (
+              <tr key={movie.imdbID}>
+                <td>{movie.Title}</td>
+                <td>{movie.Year}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="2">No movies found</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
